f27c222435fc4aefe88870b8cf3b5c4c7217f6b2
Simon <simonazike155@gmail.com>
2026-09-10 14:33:05 +0100

    fix: add new package and update to tool model

--- server/models/Tool.js
+++ server/models/Tool.js
@@ -1,28 +1,42 @@
-const toolSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const mongoose = require('mongoose');
+
+const toolSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    cat: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    icon: {
+      type: String,
+      default: '',
+      trim: true,
+      maxlength: 2,
+    },
+
+    description: {
+      type: String,
+      default: '',
+      trim: true,
+      maxlength: 140,
+    },
   },
-  url: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  cat: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  icon: {
-    type: String,
-    default: '',
-    trim: true,
-  },
-  description: {
-    type: String,
-    default: '',
-    trim: true,
-    maxlength: 140,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Tool', toolSchema);
